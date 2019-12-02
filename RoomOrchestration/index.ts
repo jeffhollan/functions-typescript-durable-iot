37c85dae7c289e5ce4d81613689a712a79db62a8
Jeff Hollan <jeffhollan@outlook.com>
2019-12-02 12:25:31 -0800

    initial

---
+++ RoomOrchestration/index.ts
@@ -0,0 +1,16 @@
+﻿import * as df from "durable-functions"
+
+const orchestrator = df.orchestrator(function* (context) {
+    let outputs = [];
+    context.log(`Someone entered the room. Starting room orchestration....`);
+    // const req = context.df.getInput();
+
+    // context.log(req);
+
+    outputs.push(yield context.df.callActivity("NotifyMeetingStart", "blah"));
+
+    return outputs;
+    
+});
+
+export default orchestrator;
